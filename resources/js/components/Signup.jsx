19a0f9948476a1cfd7b7f495259125ea8f0a0774
riki1028engi <riki1028engi@gmail.com>
2026-04-28 18:05:28 +0900

    新規登録バリデーション一時コミット

--- resources/js/components/Signup.jsx
+++ resources/js/components/Signup.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useNavigate } from "react-router-dom";
 
 import API_URL_CONST from '../constants/apiUrlConst.js';
@@ -15,21 +15,35 @@ const Signup = () => {
     }
 
     const navigate = useNavigate();
+    const isFirstRender = useRef(true);
     const [form, setForm] = useState(initialForm);
+    const [validMsgs, setValidMsgs] = useState({});
 
     const handleChange = (e) => {
         setForm({
             ...form,
             [e.target.name]: e.target.value,
-        })
+        });
     }
 
+    useEffect(() => {
+        if(isFirstRender.current) {
+            isFirstRender.current = false;
+            return;
+        }
+
+        const resultValidMsg = Bean.signupFormValidation(form);
+        setValidMsgs(resultValidMsg);
+    }, [form]);
+    
     const handleSubmit = async(e) => {
         e.preventDefault();
 
         //バリデーション
-        const validMsgs = Bean.signupFormValidation(from);
-        if(validMsgs.length > 0) {
+        const resultValidMsg = Bean.signupFormValidation(form);
+        // if(resultValidMsg.length > 0) {
+        if (Object.keys(resultValidMsg).length > 0) {
+            setValidMsgs(resultValidMsg);
             return; //バリデーションメッセージを描画する
         }
 
@@ -49,14 +63,23 @@ const Signup = () => {
                 <div className="input-box">
                     <label htmlFor="userName">ユーザー名</label>
                     <input type="text" id="userName" name="userName" onChange={handleChange}/>
+                    { (validMsgs.userName?.length > 0) && validMsgs.userName.map((msg, key) => (
+                        <div key={key}>{msg}</div>
+                    )) }
                 </div>
                 <div className="input-box">
                     <label htmlFor="loginId">ログインID</label>
                     <input type="text" id="loginId" name="loginId" onChange={handleChange}/>
+                    { (validMsgs.loginId?.length > 0) && validMsgs.loginId.map((msg, key) => (
+                        <div key={key}>{msg}</div>
+                    )) }
                 </div>
                 <div className="input-box">
                     <label htmlFor="password">パスワード</label>
                     <input type="password" id="password" name="password" onChange={handleChange}/>
+                    { (validMsgs.password?.length > 0) && validMsgs.password.map((msg, key) => (
+                        <div key={key}>{msg}</div>
+                    )) }
                 </div>
                 <div className="input-box">
                     <label htmlFor="passwordConf">確認用パスワード</label>
--- resources/js/constants/bean.jsx
+++ resources/js/constants/bean.jsx
@@ -31,60 +31,53 @@ const Bean = {
     signupFormValidation: (data) => {
 
         // バリデーションメッセージ
-        const validMesgs = {};
+        let validMesgs = {};
 
         // ユーザー名
-        if(data.userName.trim() === "" || data.userName === "undefined") {
-            validMesgs['userName'] = [
-                ...validMesgs['userName'],
-                "ユーザー名を入力してください。"
-            ]
+        if(data.userName.trim() === "" || data.userName === "undefined"
+        || data.userName.length <= 100) {
+            validMesgs = Bean.addValue(validMesgs, 'userName', 'ユーザー名は100文字以下で入力してください。');
         }
-        if (!/^[\p{Script=Han}\p{Hiragana}\p{Katakana}A-Za-z0-9０-９Ａ-Ｚａ-ｚ]+$/u.test(data.userName)) {
-            validMesgs['userName'] = [
-                ...validMesgs['userName'],
-                "漢字、ひらがな、カタカナ、英数字で入力してください。"
-            ]
+        if (!/^[\p{Script=Han}\p{Script=Hiragana}\p{Script=Katakana}a-zA-Z0-9._-]+$/u.test(data.userName)) {
+            validMesgs = Bean.addValue(validMesgs, 'userName', '漢字、ひらがな、カタカナ、英数字、記号（「.」「_」「-」）で入力してください。');
         }
 
         // ログインID
         if(data.loginId.trim() === "" || data.loginId === "undefined"
-            || data.loginId.length <= 8 || data.loginId.length >= 32) {
-            validMesgs['loginId'] = [
-                ...validMesgs['loginId'],
-                "ログインIDは8文字以上32文字以下で入力してください。"
-            ]
+        || data.loginId.length <= 8 || data.loginId.length >= 32) {
+            validMesgs = Bean.addValue(validMesgs, 'loginId', 'ログインIDは8文字以上32文字以下で入力してください。');
         }
         if(!/^[\x21-\x7E]+$/.test(data.loginId)) {
-            validMesgs['loginId'] = [
-                ...validMesgs['loginId'],
-                "ログインIDは半角英数字、記号で入力してください。"
-            ]
+            validMesgs = Bean.addValue(validMesgs, 'loginId', 'ログインIDは半角英数字、記号で入力してください。');
         }
 
         //パスワード
         if(data.password.trim() === "" || data.password === "undefined"
-            || data.password.length <= 8 || data.password.length >= 32) {
-            validMesgs['password'] = [
-                ...validMesgs['password'],
-                "パスワードは8文字以上32文字以下で入力してください。"
-            ]
+        || data.password.length <= 8 || data.password.length >= 32) {
+            validMesgs = Bean.addValue(validMesgs, 'password', 'パスワードは8文字以上32文字以下で入力してください。');
         }
         if(!/^[\x21-\x7E]+$/.test(data.password)) {
-            validMesgs['password'] = [
-                ...validMesgs['password'],
-                "パスワードは半角英数字、記号で入力してください。"
-            ]
+            validMesgs = Bean.addValue(validMesgs, 'password', 'パスワードは半角英数字、記号で入力してください。');
         }
         if(data.password !== data.passwordConf) {
-            validMesgs['password'] = [
-                ...validMesgs['password'],
-                "パスワードと確認用パスワードが一致しません。"
-            ]
+            validMesgs = Bean.addValue(validMesgs, 'password', 'パスワードと確認用パスワードが一致しません。');
         }
 
         return validMesgs;
     },
+
+    /**
+     * 連想配列内に特定のキー名が存在しなければ「key名:[]」を作成し引数の値を配列内に代入する、キー名が存在していれば、特定のキー名の配列に値を代入する
+     * 
+     * @param {Object} obj オブジェクト
+     * @param {string} key キー名
+     * @param {string} value 値
+     * @return {Object} 特定のキー名の配列に値を詰めたオブジェクト
+     */
+    addValue: (obj, key, value) => {
+        (obj[key] ??= []).push(value);
+        return obj;
+    }
 };
 
 export default Bean;
